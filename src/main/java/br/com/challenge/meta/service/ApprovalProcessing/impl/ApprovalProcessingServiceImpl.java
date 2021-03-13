cf7fd1e40a06ae99e7c005c7055f5133d608984e
Luan Gomes - Dev CeF <desenvolvimento3@cefpromotora.com.br>
2021-03-13 18:33:39 -0300

    implement method delete

--- src/main/java/br/com/challenge/meta/service/ApprovalProcessing/impl/ApprovalProcessingServiceImpl.java
+++ src/main/java/br/com/challenge/meta/service/ApprovalProcessing/impl/ApprovalProcessingServiceImpl.java
@@ -2,32 +2,48 @@ package br.com.challenge.meta.service.ApprovalProcessing.impl;
 
 import br.com.challenge.meta.dto.model.ApprovalProcessingDTO;
 import br.com.challenge.meta.filter.ApprovalProcessingFilter;
+import br.com.challenge.meta.model.ApprovalProcessing.ApprovalProcessing;
+import br.com.challenge.meta.repository.ApprovalProcessingRepository;
 import br.com.challenge.meta.service.ApprovalProcessing.ApprovalProcessingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ServerErrorException;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class ApprovalProcessingServiceImpl implements ApprovalProcessingService {
-  @Override
-  public List<ApprovalProcessingDTO> findAll(ApprovalProcessingFilter filter, Pageable pageable) {
-    return null;
-  }
 
-  @Override
-  public ApprovalProcessingDTO save(ApprovalProcessingDTO dto) {
-    return null;
-  }
+    private final ApprovalProcessingRepository repository;
 
-  @Override
-  public ApprovalProcessingDTO update(ApprovalProcessingDTO dto, UUID id) {
-    return null;
-  }
+    @Override
+    public List<ApprovalProcessingDTO> findAll(ApprovalProcessingFilter filter, Pageable pageable) {
+        return null;
+    }
 
-  @Override
-  public Boolean delete(UUID id) {
-    return null;
-  }
+    @Override
+    public ApprovalProcessingDTO save(ApprovalProcessingDTO dto) {
+        return null;
+    }
+
+    @Override
+    public ApprovalProcessingDTO update(ApprovalProcessingDTO dto, UUID id) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(UUID id) {
+        ApprovalProcessing approvalProcessingToDelete = this.repository.findById(id).orElseThrow(() -> new RuntimeException(""));
+
+        try {
+            this.repository.delete(approvalProcessingToDelete);
+        } catch (Exception e) {
+            throw new ServerErrorException("Failed delete approvalProcessing. Error: " + e.getMessage());
+        }
+
+        return true;
+    }
 }
